a4fb86f654f727364694c79e3afa4b75b581e8b5
fds_t <fdisks@protonmail.com>
2025-08-11 23:18:10 +0200

    Add warning to clean_neocities.sh

--- clean_neocitites.sh
+++ clean_neocitites.sh
@@ -6,6 +6,21 @@
 # Usage: ./clean_neocities.sh path/to/built/site/
 # (e.g., ./clean_neocities.sh build/site/ )
 
+echo "!!  CAUTION!!  CAUTION!!  CAUTION!!  CAUTION!!  CAUTION!!  CAUTION!!"
+echo "!!                                                                !!"
+echo "!! If your res/ folder is not up to date, everything newer on the !!"
+echo "!! server WILL be deleted!! Double check if anything's missing!!! !!"
+echo "!!                                                                !!"
+echo "!!  CAUTION!!  CAUTION!!  CAUTION!!  CAUTION!!  CAUTION!!  CAUTION!!"
+echo
+echo "Script will start in..."
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+echo "Cleaning site..."
 mkdir tmp
 cd tmp
 neocities pull
